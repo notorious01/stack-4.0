a5988b6da39870d7c10840a0a0b1a0505b181fb4
notorious01 <tuborg1604@gmail.com>
2017-11-12 14:06:16 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,22 +1,49 @@
-#include <iostream>
-#include <sstream>
-
 #include "stack.hpp"
+#include <string>
+#include <chrono>
+
+void producer(stack<int> &Stack)
+{
+	for(;;)
+	{
+        try
+        {
+		        Stack.push(std::rand() % 100);
+        }
+        catch(std::bad_alloc)
+        {
+                std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 1));
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 1));
+	}
+}
+
+void consumer(stack<int> &Stack)
+{
+	for(;;)
+	{
+		try
+		{
+			Stack.pop();
+		}
+		catch(std::logic_error)
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 2));
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 2));
+	}	
+}
 
 int main()
 {
-    stack<int> s;
-    for( std::string line; std::getline( std::cin, line ); ) {
-        std::istringstream stream{ line };
-        if( char op; stream >> op ) {
-            if( op == 'e' ) {
-                std::cout << std::boolalpha << s.empty();
-            } 
-            else if( op == 'q' ) {
-                break;
-            }
-        }
-    }
-    
-    return 0;
+	stack<int> Stack;
+	
+	std::thread prod(producer, std::ref(Stack));
+	std::thread cons(consumer, std::ref(Stack));
+	
+	prod.join();
+	cons.join();
+
+	
+	return 0;
 }
