9a3469ac94f3cccdc59b6052709b12347e654a72
notorious01 <tuborg1604@gmail.com>
2017-12-17 22:55:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,20 +1,62 @@
 #include <catch.hpp>
-#include "stack.hpp"
+#include <stack.hpp>
 
-TEST_CASE("push") 
+SCENARIO("stack init")
 {
-    stack<int> Stack;
-    Stack.push(5);
-    Stack.push(7);
-    REQUIRE(Stack.count() == 2);
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+}
+SCENARIO("stack init object" ) 
+{
+	stack<int> st1;
+
+	st1.push(1);
+	st1.push(2);
+	st1.push(3);
+	stack<int> st2(st1);
+	REQUIRE(st1.count() ==3 ); 
+	REQUIRE(st1.count() == st2.count());
 }
 
-TEST_CASE("pop") 
+SCENARIO("stack op=")
 {
-    stack<int> Stack;
-    Stack.push(5);
-    Stack.push(7);
-    Stack.push(9);
-    Stack.pop();
-    REQUIRE(Stack.count() == 2);
+	stack<int> st1;
+
+	st1.push(1);
+	st1.push(2);
+	st1.push(3);
+	stack<int> st2 = st1;
+	REQUIRE(st1.count() ==3 ); 
+	REQUIRE(st1.count() == st2.count() );
+}
+
+SCENARIO("stack push")
+{
+	stack<int> st;
+	REQUIRE(st.count() == 0);
+	st.push(6);
+	REQUIRE(st.count() == 1);
+	st.push(7);
+	REQUIRE(st.count() == 2);
+	st.push(78);
+	REQUIRE(st.count() == 3);
+}
+SCENARIO("pop") 
+{
+	stack<int> st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	int val = st.top();
+	st.pop();
+	REQUIRE(st.count() == 2);
+	REQUIRE(val == 3);
+	val = st.top();
+	st.pop();
+	REQUIRE(st.count() == 1);
+	REQUIRE(val == 2);
+	val = st.top();
+	st.pop();
+	REQUIRE(st.count() == 0);
+	REQUIRE(val == 1);
 }
